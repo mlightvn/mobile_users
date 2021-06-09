ead515c1547b7d6d785e2b96b08dfdda6d3f3ee4
Nguyen Ngoc Nam <nam.nguyen@playnext-lab.co.jp>
2021-06-09 18:23:41 +0900

    first commit

---
+++ App.vue
@@ -0,0 +1,35 @@
+<template>
+  <view class="container">
+    <text class="text-color-primary">{{message}}</text>
+    <button title="Press me!" @press="exclaim" />
+  </view>
+</template>
+
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
+
+<script>
+
+export default {
+  data() {
+    return {
+      name: "MLight",
+      message: "Hello World",
+    };
+  },
+  methods: {
+    exclaim() {
+      this.message += "!";
+    }
+  },
+}
+</script>
